6dc1cb8fff4653894a2fa9b13aa29cafe6f4afcd
manishsayamee <manishsayamee@gmail.com>
2020-08-20 08:55:18 +0545

    [correction of checkersignup]

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,6 @@ from .views import  SignupmakerView, SignupCheckerView,login_View, logout_view
 app_name='accounts'
 urlpatterns=[
   # accounts/signup/
-  # path('home/', SignupView.as_view(), name='home'),
   path('maker/',SignupmakerView.as_view(), name='signupmaker'),
   path('checker/',SignupCheckerView.as_view(), name='signupchecker'),
   path('login/', login_View, name='login'),
