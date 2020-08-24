0f93496d431357838b57353088dec13936bfc521
manishsayamee <manishsayamee@gmail.com>
2020-08-24 08:00:42 +0545

    Tile changed

--- task1/settings.py
+++ task1/settings.py
@@ -79,10 +79,13 @@ AUTH_USER_MODEL = 'accounts.User'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+            
     }
 }
 
@@ -122,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-import os
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
--- task1/wsgi.py
+++ task1/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task1.settings')
+
+# application = get_wsgi_application()
+
+
 import os
-
 from django.core.wsgi import get_wsgi_application
-
+from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task1.settings')
+application = Cling(get_wsgi_application())
 
-application = get_wsgi_application()
